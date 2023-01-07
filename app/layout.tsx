ce39555f357db89aa2a8350605fbbd6411c0cb0d
LucioFider <fidelislucio404@gmail.com>
2023-01-08 09:54:47 +1000

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
-import './globals.css'
+import { Footer } from "../components/footer";
+import { Header } from "../components/header";
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -12,7 +14,15 @@ export default function RootLayout({
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <div>
+          <Header />
+          <main className="overflow-hidden page-gradient pt-navigation-height">
+            {children}
+          </main>
+          <Footer />
+        </div>
+      </body>
     </html>
-  )
+  );
 }
---
+++ components/clients.tsx
@@ -0,0 +1,9 @@
+export const Clients = () => (
+  <>
+    <p>
+      Powered by the latest Cutting-Edge-Technologies.
+      <br />
+      Built for next-gen startups to established enterprises!
+    </p>
+  </>
+);
